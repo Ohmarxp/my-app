8c258aea700be263da75b61d35030e9729bbcbab
Marxho <marxhop@hotmail.com>
2023-05-18 01:31:12 -0400

    Add files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { render } from 'react-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = document.getElementById('root');
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>

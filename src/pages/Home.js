feb265dbf234c4f92a769e46652d3157400f000c
Marxho <marxhop@hotmail.com>
2023-05-18 14:11:24 -0400

    Create home page and add style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
+import  GitHubIcon from '@material-ui/icons/GitHub';
+import '../styles/Home.css';
 
 function Home() {
-    return <div>Home</div>
-    
+    return (
+    <div className='home'>
+        <div className='about'>
+            <h2> Hi, My name is Ohmarx</h2>
+            <div className='prompt'>
+                <p>A fullstack developer student with a passion for technology.
+                </p>
+                <LinkedInIcon />
+                <EmailIcon />
+                <GitHubIcon />
+            </div>
+        </div>
+        <div className='skills'>
+            <h1>Skills</h1>
+            <ol className='list'>
+                <li className='item'>
+                    <h2>Front-End</h2>
+                    <span>
+                        blah blah
+                    </span>
+                </li>
+                <li className='item'>
+                    <h2>Back-End</h2>
+                    <span></span>
+                </li>
+                <li className='item'>
+                    <h2>Languages</h2>
+                    <span></span>
+                </li>
+
+            </ol>
+        </div>
+
+    </div>
+   ) 
 }
 export default Home;

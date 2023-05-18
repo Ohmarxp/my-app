c2f18a961d7891ba6e5ac315ac68d7db5a02de36
Marxho <marxhop@hotmail.com>
2023-05-18 02:15:16 -0400

    Create link for pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import Experience from './pages/Experience'
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/Home" element={<Home />}/>
           <Route path="/Projects" element={<Projects />}/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className='toggleButton'>
+                <button></button>
+            </div>
+            <div className='links'>
+                <Link to='/Home'>Home</Link>
+                <Link to='/Projects'>Projects</Link>
+                <Link to='/Experience'>Experience</Link>
+            </div>
+        </div>
     )
 }
 export default Navbar;

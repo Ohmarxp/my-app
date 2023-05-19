ceaaadb6d7b14845e272fa307b00723f6fbe6e55
Marxho <marxhop@hotmail.com>
2023-05-18 22:35:34 -0400

    Add more content to Portfolio, modify readme file

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,6 +4,8 @@ import Proj1 from '../assets/6029974.jpg';
 import Proj2 from '../assets/372581.jpg';
 import Proj3 from '../assets/Kai1.jpg';
 import Proj4 from '../assets/Magic-Boat.jpg';
+import Proj5 from '../assets/Kaiart1.jpg';
+import Proj6 from '../assets/bricks2.jpg';
 import '../styles/Projects.css'
 
 
@@ -14,8 +16,10 @@ function Projects() {
         <div className='projectList'>
             <a href = 'https://secret-tundra-26696.herokuapp.com/' target="_blank" rel="noreferrer"><ProjectItem name='MoJobs' image={Proj1}/></a>
             <a href = 'https://bsmisson.github.io/HikR/' target="_blank" rel="noreferrer"> <ProjectItem name='HikR' image={Proj2}/></a>
-            <a href = 'https://ohmarxp.github.io/my-portfolio//' target="_blank" rel="noreferrer"> <ProjectItem name='HTML-CSS-Portfolio' image={Proj3}/></a>
-            <ProjectItem name='Magic Boat' image={Proj4}/>
+            <a href = 'https://ohmarxp.github.io/my-portfolio//' target="_blank" rel="noreferrer"><ProjectItem name='HTML-CSS-Portfolio' image={Proj3}/></a>
+            <a href = 'https://polar-hollows-15544.herokuapp.com/notes' target="_blank" rel="noreferrer"><ProjectItem name='saveMyNotes' image={Proj4}/></a>
+            <a href = 'https://sleepy-temple-38089.herokuapp.com/' target="_blank" rel="noreferrer"><ProjectItem name='iText' image={Proj5}/></a>
+            <a href = 'https://ohmarxp.github.io/greater-security/' target="_blank" rel="noreferrer"><ProjectItem name='Greater-Security' image={Proj6}/></a>
         </div>
     </div>
     )  
